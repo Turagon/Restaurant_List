4da2c98c136fc0f11933c6e60562d867fd0c159d
rexlin <rexlin6245@gmail.com>
2021-05-24 17:41:56 -0700

    app.js add new route /:restaurant_id, add card-type and detail-info html

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const restaurants = require('./restaurant.json')
+const restaurantList = restaurants.results
 const app = express()
 const port = 3000
 
-app.engine('handlebars', exphbs({defaultLayout : 'main'}))
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
 
 app.get('/home', (req, res) => {
-  res.render('main')
+  res.render('cards', { restaurant: restaurantList })
 })
+
+app.get('/search', (req, res) => {
+  res.render('cards', { restaurant: restaurantList })
+})
+
+app.get('/:restaurant_id', (req, res) => {
+  let restaurant = restaurantList.filter(item => {
+    return item.id.toString() === req.params.restaurant_id
+  })
+  res.render('detail', { restaurant: restaurant })
+})
+
 
 
 
